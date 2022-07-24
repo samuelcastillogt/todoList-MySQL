aeaa6c4e8497b4aa97d7b7b962391ece561fed83
Samuel Castillo <73082164+samuelcastillogt@users.noreply.github.com>
2022-07-23 21:15:58 -0600

    Update dbConfing.js

--- libs/dbConfing.js
+++ libs/dbConfing.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'zylowj25x51u.us-east-1.psdb.cloud',
-  user: '3aqq0h3y7xdi',
-  password: "pscale_pw_NDbobN7usV3O0X96OBiH7dqhceL51GuA_zzSTF8tIgM",
+  user: '',
+  password: "",
   database: 'todolist',
   ssl: {
     rejectUnauthorized: false
